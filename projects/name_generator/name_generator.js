e15797140698f50c6eebd558340beac1aace58b1
Christine Schatz <cmschatz@gmail.com>
2015-06-14 01:33:56 -0400

    Dog project

--- projects/name_generator/name_generator.js
+++ projects/name_generator/name_generator.js
@@ -3,6 +3,21 @@ First run at a program that generates dog names based on personality
 type. also thinking about a japanese tupperware slogan generator
 */
 
-function generateName() {
+var male_names =  {
+  cute: ["Tempura","Miso","Bonzo","Izzy"],
+  macho: ["Buster", "Scout", "Puma","Jax"],
+  martial_arts: ["Dojo","Ninja","Chi","Kendo"],
 
+}
+
+var female_names = {
+  cute: ["Cocoa","Lulu","Lily","Butter","ginger"],
+  singers: ["Aretha","Martha","Cher"],
+  hawaiian: ["Kona","Kay","Malia","Coconut","Maui"]
+}
+
+
+function generateName() {
+  console.log("Is your dog male or female?")
+  var
 }
