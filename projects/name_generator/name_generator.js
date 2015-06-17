0ce63cc489339ae12eba162f4f36eb9af9a46baf
Christine Schatz <cmschatz@gmail.com>
2015-06-16 22:42:35 -0400

    for now add js file

--- projects/name_generator/name_generator.js
+++ projects/name_generator/name_generator.js
@@ -2,16 +2,24 @@
 First run at a program that generates dog names based on personality
 type. also thinking about a japanese tupperware slogan generator
 */
+window.onload = initialize;
+
+function intialize() {
+  document.getElementById("dogSumbit").onclick = function() {
+    document.getElementById("nameDiv").innerHTML = generateName();
+    return false;
+  }
+}
 
 var male_names =  {
-  cute: ["Tempura","Miso","Bonzo","Izzy"],
-  macho: ["Buster", "Scout", "Puma","Jax"],
+  cute: ["Tempura","Miso","Bonzo","Izzy", "Snickers", "Waffy"],
+  macho: ["Buster", "Scout", "Puma","Jax","Rhino"],
   martial_arts: ["Dojo","Ninja","Chi","Kendo"],
 
 }
 
 var female_names = {
-  cute: ["Cocoa","Lulu","Lily","Butter","ginger"],
+  cute: ["Cocoa","Lulu","Lily","Butter","Ginger", "Bubble", "Muffin"],
   singers: ["Aretha","Martha","Cher"],
   hawaiian: ["Kona","Kay","Malia","Coconut","Maui"]
 }
