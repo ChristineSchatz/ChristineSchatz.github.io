86ee4c063fe4018cd35b1a64c47e1f0a23397051
ChristineSchatz <cmschatz@gmail.com>
2015-07-24 12:16:33 -0400

    Init game commit

---
+++ projects/forest_game/forest.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+  var move1 = 0;
+  var move2 = 0;
+
+   $('#begin').on('click',function() {
+    $(this).hide();
+     move1 = 0;
+     move2 = 0;
+     $('#playerA').css('margin-left',"0")
+     $('#playerB').css('margin-left',"0")
+
+    $('#track1').on('click', $('.deer'), function(event) {
+      $(this).hide();
+    })
+
+
+
+    // $(document).on('keyup', function(event) {
+    //   if(event.keyCode == 74) {
+    //     if(move1 < 100) {
+    //       move1 += 2;
+    //       $('#playerA').css('margin-left',move1 + "%") }
+    //       else {
+    //         alert ("Game over!, Player B wins!");
+    //         $(document).unbind('keyup')
+    //       }
+    //     }
+    //     if(event.keyCode == 75) {
+    //       if(move2 < 100) {
+    //         move2 += 2;
+    //         $('#playerB').css('margin-left', move2 + "%")}
+    //         else {
+    //           alert("Game over, Player B wins!");
+    //           $(document).unbind('keyup')
+    //         }
+    //       }
+
+    //     })
+      })
+    })
