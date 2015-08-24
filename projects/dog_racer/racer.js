7efaccfd178e63451af9806c838e8bea4329bf3f
Christine Schatz <cmschatz@gmail.com>
2015-08-23 22:40:51 -0400

    Set up dog racer game

---
+++ projects/dog_racer/racer.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+   $('#begin').on('click',function() {
+    $(this).hide();
+     move1 = 0;
+     move2 = 0;
+     $('#playerA').css('margin-left',"0")
+     $('#playerB').css('margin-left',"0")
+
+    $(document).on('keyup', function(event) {
+      if(event.keyCode == 74) {
+        if(move1 < 100) {
+          move1 += 2;
+          $('#playerA').css('margin-left',move1 + "%") }
+          else {
+            alert ("Game over!, Player B wins!");
+            $(document).unbind('keyup')
+          }
+        }
+        if(event.keyCode == 75) {
+          if(move2 < 100) {
+            move2 += 2;
+            $('#playerB').css('margin-left', move2 + "%")}
+            else {
+              alert("Game over, Player B wins!");
+              $(document).unbind('keyup')
+            }
+          }
+
+        })
+      })
+    })
+
+function displayWinner() {
+
+}
