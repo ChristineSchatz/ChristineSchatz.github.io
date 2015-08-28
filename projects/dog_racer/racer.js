dfed64d4a11a29a43cff4e48d8dc8e8c806ab727
Christine Schatz <cmschatz@gmail.com>
2015-08-27 22:24:33 -0400

    Fix header

--- projects/dog_racer/racer.js
+++ projects/dog_racer/racer.js
@@ -9,35 +9,28 @@ $(document).ready(function() {
 
    $('#begin').on('click',function() {
     $(this).hide();
-     // move1 = 0;
-     // move2 = 0;
      $(playerA.name).css('margin-left',"0")
      $(playerB.name).css('margin-left',"0")
 
     $(document).on('keyup', function(event) {
       if(event.keyCode == 74) {
         if(playerA.posX < 100) {
-          // move1 += 2;
           updatePosition(playerA)
         }
-          // $('#playerA').css('margin-left',move1 + "%") }
-          else {
-            alert ("Game over!, Player B wins!");
-            $(document).unbind('keyup')
+        else {
+          alert ("Game over!, Player B wins!");
+          $(document).unbind('keyup')
           }
         }
         if(event.keyCode == 75) {
           if(playerB.posX < 100) {
             updatePosition(playerB)
           }
-            // move2 += 2;
-            // $('#playerB').css('margin-left', move2 + "%")}
-            else {
-              alert("Game over, Player B wins!");
-              $(document).unbind('keyup')
+          else {
+            alert("Game over, Player B wins!");
+            $(document).unbind('keyup')
             }
           }
-
         })
       });
     });
